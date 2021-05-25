a2e95e5ddf7f985bac443ea3d85d78c455c31a6c
Gabriel-P22 <paschoalfilho0303@gmail.com>
2021-05-24 23:53:17 -0300

    feat: part one project consult

---
+++ src/main/java/com/github/gabrielp22/citiesapi/countries/repository/CountryRepository.java
@@ -0,0 +1,8 @@
+package com.github.gabrielp22.citiesapi.countries.repository;
+
+import com.github.gabrielp22.citiesapi.countries.Country;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CountryRepository extends JpaRepository<Country, Long>{
+
+}
